915f249500fbd4526902656db0ddcaadf5062ff1
Fidelixjv <joaovitorfidelix852@gmail.com>
2025-05-29 08:42:31 -0300

    questoes

---
+++ q042/042.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina de alerta</title>
+</head>
+<body>
+    <h1 style="text-align: center">Bem-vindo</h1>
+
+    <?php
+    include 'alerta.php';
+    ?>
+</body>
+</html>
---
+++ q042/alerta.php
@@ -0,0 +1,11 @@
+<?php
+echo "<div style=
+    'padding: 10px;
+    background-color: red;
+    border-radius: 10px;
+    margin: 10px;
+    border: 2px solid black;
+    text-align: center;
+    '> 
+    alerta: este é um aviso de alerta!
+    </div>";
